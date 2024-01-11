63018386e46ec1651a1c9cc6f6de7b38738150d5
krone <krone@Mac-mini-de-krone.local>
2024-01-10 21:01:04 -0300

    add macOS support

--- install.sh
+++ install.sh
@@ -3,12 +3,32 @@ GOARCH=$(go env GOARCH)
 
 function compile(){
   GOOS=$GOOS GOARCH=$GOARCH go build main.go
+  if [ "$(uname)" == "Darwin" ]; then
+    # Se o sistema é macOS, ajuste GOOS e GOARCH conforme necessário
+    GOOS=darwin
+    GOARCH=amd64 # ou arm64 para Macs com chip M1
+  fi
+  GOOS=$GOOS GOARCH=$GOARCH go build main.go
 }
+
+function install_macos(){
+  mv main /usr/local/bin/goanime
+  ln -sf /usr/local/bin/goanime /usr/local/bin/go-anime
+}
+
+function install_others(){
+  mv main /usr/bin/goanime
+  ln -sf /usr/bin/goanime /usr/bin/go-anime
+}
+
 
 function start(){
   compile
-  mv main /usr/local/bin/goanime
-  ln -sf /usr/local/bin/goanime /usr/bin/go-anime
+  if [ "$(uname)" == "Darwin" ]; then
+    install_macos
+  else
+    install_others
+  fi
 }
 
 if [ "$EUID" -eq 0 ]; then
@@ -16,3 +36,4 @@ if [ "$EUID" -eq 0 ]; then
 else
   echo "Este programa deve ser rodado como sudo"
 fi
+
