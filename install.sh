25e923cf568b46fe417949b44867293e83c0ee0b
alvarorichard <alvaro10048@protonmail.com>
2024-01-18 20:00:27 -0300

    update script install

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
-GOOS=$(go env GOOS)
-GOARCH=$(go env GOARCH)
+#!/bin/bash
+
+# Assuming the Go binary is in /usr/local/go/bin
+GO_PATH="/usr/local/go/bin/go"
+
+GOOS=$($GO_PATH env GOOS)
+GOARCH=$($GO_PATH env GOARCH)
+
+if [ "$(uname)" == "Darwin" ]; then
+    GOOS=darwin
+    GOARCH=amd64 # or arm64 for M1 Macs
+fi
 
 function compile(){
-  GOOS=$GOOS GOARCH=$GOARCH go build main.go
-  if [ "$(uname)" == "Darwin" ]; then
-    # Se o sistema é macOS, ajuste GOOS e GOARCH conforme necessário
-    GOOS=darwin
-    GOARCH=amd64 # ou arm64 para Macs com chip M1
-  fi
-  GOOS=$GOOS GOARCH=$GOARCH go build main.go
+  # Using the full path for the Go executable
+  GOOS=$GOOS GOARCH=$GOARCH $GO_PATH build main.go
 }
-# add bin to path macOS only
+
 function install_macos(){
   mv main /usr/local/bin/goanime
   ln -sf /usr/local/bin/goanime /usr/local/bin/go-anime
@@ -20,7 +25,6 @@ function install_others(){
   mv main /usr/bin/goanime
   ln -sf /usr/bin/goanime /usr/bin/go-anime
 }
-
 
 function start(){
   compile
@@ -35,5 +39,6 @@ if [ "$EUID" -eq 0 ]; then
   start
 else
   echo "Este programa deve ser rodado como sudo"
+  # Optionally add an English translation
+  echo "This program must be run as sudo"
 fi
-
