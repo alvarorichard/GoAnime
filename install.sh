519773a3090cc265e96c8e3f24ca081595711212
alvarorichard <alvaro10048@protonmail.com>
2024-01-19 00:31:29 -0300

    fix install

--- install.sh
+++ install.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 
-# Find the full path of the Go executable
-GO_PATH=$(which go)
-
-# Check if Go is installed
-if [ -z "$GO_PATH" ]; then
-    echo "Go is not installed or not in the PATH"
-    exit 1
-fi
-
-# Get GOOS and GOARCH
-GOOS=$($GO_PATH env GOOS)
-GOARCH=$($GO_PATH env GOARCH)
-
-# Determine the installation directory
-if [ -w /usr/local/bin ]; then
-    INSTALL_DIR="/usr/local/bin"
-else
-    INSTALL_DIR="/usr/bin"
-fi
-
-if [ "$(uname)" == "Darwin" ]; then
-    GOOS=darwin
-    GOARCH=amd64 # or arm64 for M1 Macs
-fi
+GOOS=$(go env GOOS)
+GOARCH=$(go env GOARCH)
 
 function compile(){
-  # Use the full path for the Go executable
-  GOOS=$GOOS GOARCH=$GOARCH $GO_PATH build main.go
+  GOOS=$GOOS GOARCH=$GOARCH go build main.go
+  if [ "$(uname)" == "Darwin" ]; then
+    # Se o sistema é macOS, ajuste GOOS e GOARCH conforme necessário
+    GOOS=darwin
+    GOARCH=amd64 # ou arm64 para Macs com chip M1
+  fi
+  GOOS=$GOOS GOARCH=$GOARCH go build main.go
 }
-
+# add bin to path macOS only
 function install_macos(){
   mv main /usr/local/bin/goanime
   ln -sf /usr/local/bin/goanime /usr/local/bin/go-anime
@@ -39,6 +22,7 @@ function install_others(){
   mv main /usr/bin/goanime
   ln -sf /usr/bin/goanime /usr/bin/go-anime
 }
+
 
 function start(){
   compile
@@ -53,6 +37,42 @@ if [ "$EUID" -eq 0 ]; then
   start
 else
   echo "Este programa deve ser rodado como sudo"
-  # Optionally add an English translation
-  echo "This program must be run as sudo"
+fi
+GOOS=$(go env GOOS)
+GOARCH=$(go env GOARCH)
+
+function compile(){
+  GOOS=$GOOS GOARCH=$GOARCH go build main.go
+  if [ "$(uname)" == "Darwin" ]; then
+    # Se o sistema é macOS, ajuste GOOS e GOARCH conforme necessário
+    GOOS=darwin
+    GOARCH=amd64 # ou arm64 para Macs com chip M1
+  fi
+  GOOS=$GOOS GOARCH=$GOARCH go build main.go
+}
+# add bin to path macOS only
+function install_macos(){
+  mv main /usr/local/bin/goanime
+  ln -sf /usr/local/bin/goanime /usr/local/bin/go-anime
+}
+
+function install_others(){
+  mv main /usr/bin/goanime
+  ln -sf /usr/bin/goanime /usr/bin/go-anime
+}
+
+
+function start(){
+  compile
+  if [ "$(uname)" == "Darwin" ]; then
+    install_macos
+  else
+    install_others
+  fi
+}
+
+if [ "$EUID" -eq 0 ]; then
+  start
+else
+  echo "Este programa deve ser rodado como sudo"
 fi
