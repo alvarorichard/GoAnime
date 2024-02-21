fb536291ea1fb49211660316175ade6447731c0b
alvarorichard <alvaro10048@protonmaill.com>
2024-02-20 23:11:26 -0300

    add mitigation for go_injection_rule-ssrf cleam

--- main.go
+++ main.go
@@ -115,33 +115,6 @@ func SafeGet(url string) (*http.Response, error) {
 }
 
 
-
-// cuidado
-//func extractVideoURL(url string) (string, error) {
-//	response, err := http.Get(url)
-//	if err != nil {
-//		return "", fmt.Errorf("failed to fetch URL: %v", err)
-//	}
-//	defer response.Body.Close()
-
-//	doc, err := goquery.NewDocumentFromReader(response.Body)
-//	if err != nil {
-//		return "", fmt.Errorf("failed to parse HTML: %v", err)
-//	}
-
-//	videoElements := doc.Find("video")
-//	if videoElements.Length() == 0 {
-//		videoElements = doc.Find("div")
-//	}
-
-//	if videoElements.Length() == 0 {
-//		return "", errors.New("no video elements found in the HTML")
-//	}
-
-//	videoSrc, _ := videoElements.Attr("data-video-src")
-//	return videoSrc, nil
-//}
-
 func extractVideoURL(url string) (string, error) {
 	response, err := SafeGet(url)
 	if err != nil {
@@ -167,44 +140,6 @@ func extractVideoURL(url string) (string, error) {
 	return videoSrc, nil
 }
 
-
-
-
-// cuidado 
-/*func extractActualVideoURL(videoSrc string) (string, error) {
-    response, err := http.Get(videoSrc)
-    if err != nil {
-        return "", fmt.Errorf("failed to fetch video source: %v", err)
-    }
-    defer response.Body.Close()
-
-    if response.StatusCode != http.StatusOK {
-        return "", fmt.Errorf("request failed with status: %s", response.Status)
-    }
-
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        return "", fmt.Errorf("failed to read response body: %v", err)
-    }
-
-    var videoResponse VideoResponse
-    if err := json.Unmarshal(body, &videoResponse); err != nil {
-        return "", fmt.Errorf("failed to unmarshal JSON response: %v", err)
-    }
-
-    if len(videoResponse.Data) == 0 {
-        return "", errors.New("no video data found in the response")
-    }
-
-    // Function to compare video quality labels and return the highest quality video URL
-    highestQualityVideoURL := selectHighestQualityVideo(videoResponse.Data)
-    if highestQualityVideoURL == "" {
-        return "", errors.New("no suitable video quality found")
-    }
-
-    return highestQualityVideoURL, nil
-}
-//*/
 
 func extractActualVideoURL(videoSrc string) (string, error) {
 	response, err := SafeGet(videoSrc)
@@ -240,7 +175,6 @@ func extractActualVideoURL(videoSrc string) (string, error) {
 	return highestQualityVideoURL, nil
 }
 
-
 // Assumes that the quality label contains resolution information (e.g., "1080p").
 // This function can be adapted based on the actual format of the quality labels.
 func selectHighestQualityVideo(videos []VideoData) string {
@@ -263,19 +197,6 @@ func isHigherQuality(quality1, quality2 string) bool {
     return quality1Value > quality2Value
 }
 
-
-// func PlayVideo(videoURL string) error {
-// 	cmd := exec.Command("vlc", "-vvv", videoURL)
-// 	if err := cmd.Start(); err != nil {
-// 		return fmt.Errorf("failed to start video player: %v", err)
-// 	}
-
-// 	if err := cmd.Wait(); err != nil {
-// 		return fmt.Errorf("failed to play video: %v", err)
-// 	}
-
-// 	return nil
-// }
 
 func PlayVideo(videoURL string, episodes []Episode, currentEpisodeIndex int) error {
 	var wg sync.WaitGroup
@@ -352,7 +273,6 @@ func getVideoURLForEpisode(episodeURL string) (string, error) {
 	}
 	return extractActualVideoURL(videoURL)
 }
-
 
 
 func selectWithGoFuzzyFinder(items []string) (string, error) {
