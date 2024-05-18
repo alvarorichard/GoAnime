5116a1823d909e9b23caf5f5b44c0faaef3ae713
alvarorichard <alvaro10048@protonmail.com>
2024-05-18 01:45:57 -0300

    reduce SLoC and complexity

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,72 +1,3 @@
-//package api
-//
-//import (
-//	"crypto/tls"
-//	"net"
-//	"net/http"
-//	"time"
-//
-//	"github.com/pkg/errors"
-//	"golang.org/x/net/context"
-//)
-//
-//// IsDisallowedIP checks if the given IP is disallowed.
-//func IsDisallowedIP(hostIP string) bool {
-//	ip := net.ParseIP(hostIP)
-//	return ip.IsMulticast() || ip.IsUnspecified() || ip.IsLoopback() || ip.IsPrivate()
-//}
-//
-//// customDial checks if the remote IP is allowed and returns a connection.
-//func customDial(network, addr string, timeout time.Duration) (net.Conn, error) {
-//	c, err := net.DialTimeout(network, addr, timeout)
-//	if err != nil {
-//		return nil, err
-//	}
-//	ip, _, _ := net.SplitHostPort(c.RemoteAddr().String())
-//	if IsDisallowedIP(ip) {
-//		return nil, errors.New("ip address is not allowed")
-//	}
-//	return c, nil
-//}
-//
-//// customDialTLS checks if the remote IP is allowed and performs TLS handshake.
-//func customDialTLS(network, addr string, timeout time.Duration) (net.Conn, error) {
-//	dialer := &net.Dialer{Timeout: timeout}
-//	c, err := tls.DialWithDialer(dialer, network, addr, &tls.Config{
-//		MinVersion: tls.VersionTLS12,
-//	})
-//	if err != nil {
-//		return nil, err
-//	}
-//	ip, _, _ := net.SplitHostPort(c.RemoteAddr().String())
-//	if IsDisallowedIP(ip) {
-//		return nil, errors.New("ip address is not allowed")
-//	}
-//	return c, c.Handshake()
-//}
-//
-//// SafeTransport returns a http.Transport with custom dial functions.
-//func SafeTransport(timeout time.Duration) *http.Transport {
-//	return &http.Transport{
-//		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-//			return customDial(network, addr, timeout)
-//		},
-//		DialTLS: func(network, addr string) (net.Conn, error) {
-//			return customDialTLS(network, addr, timeout)
-//		},
-//		TLSHandshakeTimeout: timeout,
-//	}
-//}
-//
-//// SafeGet performs a GET request with a timeout and returns the response.
-//func SafeGet(url string) (*http.Response, error) {
-//	const clientConnectTimeout = time.Second * 10
-//	httpClient := &http.Client{
-//		Transport: SafeTransport(clientConnectTimeout),
-//	}
-//	return httpClient.Get(url)
-//}
-
 package api
 
 import (
@@ -85,39 +16,44 @@ func IsDisallowedIP(hostIP string) bool {
 	return ip.IsMulticast() || ip.IsUnspecified() || ip.IsLoopback() || ip.IsPrivate()
 }
 
-// customDial checks if the remote IP is allowed and returns a connection.
-func customDial(network, addr string, timeout time.Duration) (net.Conn, error) {
-	c, err := net.DialTimeout(network, addr, timeout)
-	if err != nil {
-		return nil, err
+// checkDisallowedIP validates the IP of a connection.
+func checkDisallowedIP(conn net.Conn) error {
+	ip, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
+	if IsDisallowedIP(ip) {
+		conn.Close()
+		return errors.New("ip address is not allowed")
 	}
-	if ip, _, _ := net.SplitHostPort(c.RemoteAddr().String()); IsDisallowedIP(ip) {
-		return nil, errors.New("ip address is not allowed")
-	}
-	return c, nil
+	return nil
 }
 
-// customDialTLS checks if the remote IP is allowed and performs TLS handshake.
-func customDialTLS(network, addr string, timeout time.Duration) (net.Conn, error) {
+// dialFunc handles both regular and TLS connections.
+func dialFunc(network, addr string, timeout time.Duration, tlsConfig *tls.Config) (net.Conn, error) {
 	dialer := &net.Dialer{Timeout: timeout}
-	c, err := tls.DialWithDialer(dialer, network, addr, &tls.Config{MinVersion: tls.VersionTLS12})
+	var conn net.Conn
+	var err error
+	if tlsConfig != nil {
+		conn, err = tls.DialWithDialer(dialer, network, addr, tlsConfig)
+	} else {
+		conn, err = dialer.Dial(network, addr)
+	}
 	if err != nil {
 		return nil, err
 	}
-	if ip, _, _ := net.SplitHostPort(c.RemoteAddr().String()); IsDisallowedIP(ip) {
-		return nil, errors.New("ip address is not allowed")
+	if err := checkDisallowedIP(conn); err != nil {
+		return nil, err
 	}
-	return c, c.Handshake()
+	return conn, nil
 }
 
 // SafeTransport returns a http.Transport with custom dial functions.
 func SafeTransport(timeout time.Duration) *http.Transport {
+	tlsConfig := &tls.Config{MinVersion: tls.VersionTLS12}
 	return &http.Transport{
 		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			return customDial(network, addr, timeout)
+			return dialFunc(network, addr, timeout, nil)
 		},
-		DialTLS: func(network, addr string) (net.Conn, error) {
-			return customDialTLS(network, addr, timeout)
+		DialTLSContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return dialFunc(network, addr, timeout, tlsConfig)
 		},
 		TLSHandshakeTimeout: timeout,
 	}
