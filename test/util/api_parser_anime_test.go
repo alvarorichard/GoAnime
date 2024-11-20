f6d64609d148cf4a88cd91456c25389378b34782
alvarorichard <alvaro10048@protonmail.com>
2024-11-20 02:33:17 -0300

    remove unused code

--- test/util/api_parser_anime_test.go
+++ test/util/api_parser_anime_test.go
@@ -9,30 +9,6 @@ import (
 	"testing"
 )
 
-//func TestParseAnimes(t *testing.T) {
-//	html := `
-//		<div class="row ml-1 mr-1">
-//			<a href="/anime/1">Anime One</a>
-//			<a href="/anime/2">Anime Two</a>
-//			<a href="/anime/3">Anime Three</a>
-//		</div>
-//	`
-//	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
-//	if err != nil {
-//		t.Fatalf("Failed to create document from HTML string: %v", err)
-//	}
-//
-//	expectedAnimes := []api.Anime{
-//		{Name: "Anime One", URL: "/anime/1"},
-//		{Name: "Anime Two", URL: "/anime/2"},
-//		{Name: "Anime Three", URL: "/anime/3"},
-//	}
-//
-//	animes := api.ParseAnimes(doc)
-//
-//	assert.Equal(t, expectedAnimes, animes, "Parsed animes do not match expected animes")
-//}
-
 func TestParseAnimes(t *testing.T) {
 	html := `
         <div class="row ml-1 mr-1">
