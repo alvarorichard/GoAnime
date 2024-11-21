7bf22a3cedc3e70db8a5f2272ad4543edf7d7f04
Lin Vieira <lin@fnxln.com>
2024-11-21 18:53:17 -0300

    Add support for Nix/NixOS

---
+++ flake.nix
@@ -0,0 +1,40 @@
+
+{ description = "Go Anime Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*.tar.gz";
+    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, nixos-unstable }:
+    let
+      allSystems = [
+        "x86_64-linux" 
+        "aarch64-linux" 
+        "x86_64-darwin" 
+        "aarch64-darwin" 
+      ];
+
+      
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        
+        pkgs = import nixos-unstable { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildGoModule {
+          name = "GoAnime";
+          go = pkgs.go_1_23; 
+          src = self;
+          vendorHash = "sha256-dqfgiBMcEhq5hr524BKIbP0ulByWJa7gkoxSy4598v8=";
+          subPackages = [ "cmd/goanime" ];
+          propagatedBuildInputs = with pkgs;[ mpv yt-dlp ]; 
+        };
+      });
+      devShell = forAllSystems ({ pkgs }: pkgs.mkShell {
+        buildInputs = with pkgs;[ mpv yt-dlp];
+      });
+    };
+}
+
+
