333c3662fa8e89a025b5db42f4f51af16d6d574b
alvarorichard <alvaro10048@protonmail.com>
2025-06-01 19:10:57 -0300

    refactor: remove unused functions and improve HTTP request handling

--- internal/api/anime.go
+++ internal/api/anime.go
@@ -22,115 +22,6 @@ import (
 // Common HTTP client instance
 var httpClient = &http.Client{}
 
-// func SearchAnime(animeName string) (*models.Anime, error) {
-// 	start := time.Now()
-// 	if util.IsDebug {
-// 		log.Printf("[PERF] SearchAnime iniciado para %s", animeName)
-// 	}
-
-// 	currentPageURL := fmt.Sprintf("%s/pesquisar/%s", models.AnimeFireURL, url.PathEscape(animeName))
-
-// 	if util.IsDebug {
-// 		log.Printf("Searching for anime with URL: %s", currentPageURL)
-// 	}
-
-// 	for {
-// 		selectedAnime, nextPageURL, err := searchAnimeOnPage(currentPageURL)
-// 		if err != nil {
-// 			log.Printf("[PERF] SearchAnime falhou para %s em %v", animeName, time.Since(start))
-// 			return nil, err
-// 		}
-// 		if selectedAnime != nil {
-
-// 			aniListInfo, err := FetchAnimeFromAniList(selectedAnime.Name)
-// 			if err != nil {
-// 				log.Printf("Error fetching additional data from AniList: %v", err)
-// 			} else {
-// 				selectedAnime.AnilistID = aniListInfo.Data.Media.ID
-// 				selectedAnime.MalID = aniListInfo.Data.Media.IDMal
-// 				selectedAnime.Details = aniListInfo.Data.Media
-
-// 				if aniListInfo.Data.Media.CoverImage.Large != "" {
-// 					selectedAnime.ImageURL = aniListInfo.Data.Media.CoverImage.Large
-// 					if util.IsDebug {
-// 						log.Printf("Cover image URL retrieved from AniList: %s", selectedAnime.ImageURL)
-// 					}
-// 				} else {
-// 					log.Printf("Cover image URL not found in AniList response for anime: %s", selectedAnime.Name)
-// 				}
-// 				if util.IsDebug {
-// 					log.Printf("AniList ID: %d, MAL ID: %d, Title: %s, Score: %d, Cover Image URL: %s",
-// 						aniListInfo.Data.Media.ID, aniListInfo.Data.Media.IDMal,
-// 						aniListInfo.Data.Media.Title.Romaji, aniListInfo.Data.Media.AverageScore,
-// 						selectedAnime.ImageURL)
-// 				}
-
-// 			}
-// 			if util.IsDebug {
-// 				log.Printf("[PERF] SearchAnime finalizado para %s em %v", animeName, time.Since(start))
-
-// 			}
-
-// 			return selectedAnime, nil
-// 		}
-
-// 		if nextPageURL == "" {
-// 			if util.IsDebug {
-// 				log.Printf("[PERF] SearchAnime não encontrou resultados para %s em %v", animeName, time.Since(start))
-
-// 			}
-
-// 			return nil, errors.New("no anime found with the given name")
-// 		}
-// 		currentPageURL = models.AnimeFireURL + nextPageURL
-// 	}
-// }
-
-// func searchAnimeOnPage(pageURL string) (*models.Anime, string, error) {
-// 	response, err := getHTTPResponse(pageURL)
-// 	if err != nil {
-// 		return nil, "", errors.Wrap(err, "failed to perform search request")
-// 	}
-// 	defer func(Body io.ReadCloser) {
-// 		err := Body.Close()
-// 		if err != nil {
-// 			log.Printf("Error closing response body: %v", err)
-// 		}
-// 	}(response.Body)
-
-// 	if response.StatusCode != http.StatusOK {
-// 		if response.StatusCode == http.StatusForbidden {
-// 			return nil, "", errors.New("connection refused: you need to be in Brazil or use a VPN to access the server")
-// 		}
-// 		return nil, "", errors.Errorf("search failed, server returned: %s", response.Status)
-// 	}
-
-// 	doc, err := goquery.NewDocumentFromReader(response.Body)
-// 	if err != nil {
-// 		return nil, "", errors.Wrap(err, "failed to parse response")
-// 	}
-
-// 	animes := ParseAnimes(doc)
-// 	if util.IsDebug {
-// 		log.Printf("Number of animes found: %d", len(animes))
-// 	}
-
-// 	if len(animes) > 0 {
-// 		selectedAnime, err := selectAnimeWithGoFuzzyFinder(animes)
-// 		if err != nil {
-// 			return nil, "", err
-// 		}
-// 		return selectedAnime, "", nil
-// 	}
-
-// 	nextPage, exists := doc.Find(".pagination .next a").Attr("href")
-// 	if !exists {
-// 		return nil, "", nil
-// 	}
-
-// 	return nil, nextPage, nil
-// }
-
 func GetEpisodeData(animeID int, episodeNo int, anime *models.Anime) error {
 
 	url := fmt.Sprintf("https://api.jikan.moe/v4/anime/%d/episodes/%d", animeID, episodeNo)
@@ -234,31 +125,6 @@ func GetMovieData(animeID int, anime *models.Anime) error {
 	return nil
 }
 
-// // ParseAnimes extracts a list of Anime structs from the search results page.
-// func ParseAnimes(doc *goquery.Document) []models.Anime {
-// 	var animes []models.Anime
-
-// 	doc.Find(".row.ml-1.mr-1 a").Each(func(i int, s *goquery.Selection) {
-// 		urlPath, exists := s.Attr("href")
-// 		if !exists {
-// 			return
-// 		}
-// 		url := resolveURL(models.AnimeFireURL, urlPath)
-
-// 		name := strings.TrimSpace(s.Text())
-
-// 		if util.IsDebug {
-// 			log.Printf("Parsed Anime - Name: %s, URL: %s", name, url)
-// 		}
-
-// 		animes = append(animes, models.Anime{
-// 			Name: name,
-// 			URL:  url,
-// 		})
-// 	})
-
-// 	return animes
-// }
 
 // FetchAnimeDetails retrieves additional information for the selected anime
 func FetchAnimeDetails(anime *models.Anime) error {
@@ -291,200 +157,7 @@ func FetchAnimeDetails(anime *models.Anime) error {
 	return nil
 }
 
-// func FetchAnimeFromAniList(animeName string) (*models.AniListResponse, error) {
-// 	cleanedName := CleanTitle(animeName)
-// 	if util.IsDebug {
-// 		log.Printf("Attempting AniList search with title: %s", cleanedName)
 
-// 	}
-
-// 	query := `
-//     query ($search: String) {
-//         Media(search: $search, type: ANIME) {
-//             id
-//             title { romaji english }
-//             description
-//             genres
-//             averageScore
-//             episodes
-//             status
-//             idMal
-//             coverImage { large medium }
-//         }
-//     }`
-
-// 	variables := map[string]interface{}{
-// 		"search": cleanedName,
-// 	}
-
-// 	requestBody := map[string]interface{}{
-// 		"query":     query,
-// 		"variables": variables,
-// 	}
-
-// 	jsonData, err := json.Marshal(requestBody)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to marshal request body: %v", err)
-// 	}
-
-// 	req, err := http.NewRequest("POST", "https://graphql.anilist.co", strings.NewReader(string(jsonData)))
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create request: %v", err)
-// 	}
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to fetch data from AniList API: %v", err)
-// 	}
-// 	defer func(Body io.ReadCloser) {
-// 		err := Body.Close()
-// 		if err != nil {
-// 			log.Printf("Error closing AniList response body: %v", err)
-// 		}
-// 	}(resp.Body)
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		body, _ := io.ReadAll(resp.Body)
-// 		return nil, fmt.Errorf("AniList API request failed with status %d: %s", resp.StatusCode, string(body))
-// 	}
-
-// 	var result models.AniListResponse
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to read AniList API response: %v", err)
-// 	}
-
-// 	if err := json.Unmarshal(body, &result); err != nil {
-// 		return nil, fmt.Errorf("failed to parse AniList API response: %v", err)
-// 	}
-
-// 	if result.Data.Media.ID == 0 {
-// 		log.Printf("No results found on AniList for anime: %s", cleanedName)
-// 		return nil, fmt.Errorf("no results found on AniList for anime: %s", cleanedName)
-// 	}
-
-// 	if util.IsDebug {
-// 		log.Printf("AniList ID: %d, MAL ID: %d, Title: %s, Score: %d, Cover Image URL: %s",
-// 			result.Data.Media.ID, result.Data.Media.IDMal,
-// 			result.Data.Media.Title.Romaji, result.Data.Media.AverageScore,
-// 			result.Data.Media.CoverImage.Large)
-// 	}
-
-// 	return &result, nil
-// }
-
-// // selectAnimeWithGoFuzzyFinder allows the user to select an anime from a list using fuzzy search
-// func selectAnimeWithGoFuzzyFinder(animes []models.Anime) (*models.Anime, error) {
-// 	if len(animes) == 0 {
-// 		return nil, errors.New("no anime provided")
-// 	}
-
-// 	sortedAnimes := sortAnimes(animes)
-// 	idx, err := fuzzyfinder.Find(
-// 		sortedAnimes,
-// 		func(i int) string {
-// 			return sortedAnimes[i].Name
-// 		},
-// 	)
-// 	if err != nil {
-// 		return nil, errors.Wrap(err, "failed to select anime with go-fuzzyfinder")
-// 	}
-
-// 	if idx < 0 || idx >= len(sortedAnimes) {
-// 		return nil, errors.New("invalid index returned by fuzzyfinder")
-// 	}
-
-// 	return &sortedAnimes[idx], nil
-// }
-
-// // sortAnimes sorts a list of Anime structs alphabetically by name
-// func sortAnimes(animeList []models.Anime) []models.Anime {
-// 	sort.Slice(animeList, func(i, j int) bool {
-// 		return animeList[i].Name < animeList[j].Name
-// 	})
-// 	return animeList
-// }
-
-// // resolveURL resolves relative URLs to absolute URLs based on the base URL.
-// func resolveURL(base, ref string) string {
-// 	baseURL, err := url.Parse(base)
-// 	if err != nil {
-// 		return ""
-// 	}
-// 	refURL, err := url.Parse(ref)
-// 	if err != nil {
-// 		return ""
-// 	}
-// 	return baseURL.ResolveReference(refURL).String()
-// }
-
-// // CleanTitle removes unwanted words, numbers, and ratings for better API search results
-// func CleanTitle(title string) string {
-// 	re := regexp.MustCompile(`(?i)(dublado|legendado|todos os episodios)`)
-// 	title = re.ReplaceAllString(title, "")
-
-// 	re = regexp.MustCompile(`\s+\d+(\.\d+)?\s+A\d+$`)
-// 	title = re.ReplaceAllString(title, "")
-
-// 	title = strings.TrimSpace(title)
-// 	return title
-// }
-
-// func makeGetRequest(url string, headers map[string]string) (map[string]interface{}, error) {
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create GET request: %w", err)
-// 	}
-
-// 	for key, value := range headers {
-// 		req.Header.Set(key, value)
-// 	}
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to send GET request: %w", err)
-// 	}
-// 	defer func(Body io.ReadCloser) {
-// 		err := Body.Close()
-// 		if err != nil {
-// 			fmt.Printf("failed to close response body: %v", err)
-// 		}
-// 	}(resp.Body)
-
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to read response body: %w", err)
-// 	}
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		return nil, fmt.Errorf("failed with status %d: %s", resp.StatusCode, body)
-// 	}
-
-// 	var responseData map[string]interface{}
-// 	err = json.Unmarshal(body, &responseData)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
-// 	}
-
-// 	return responseData, nil
-// }
-
-// func getHTTPResponse(url string) (*http.Response, error) {
-// 	client := &http.Client{}
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return resp, nil
-// }
 
 func SearchAnime(animeName string) (*models.Anime, error) {
 	start := time.Now()
@@ -741,7 +414,20 @@ func httpPost(url string, body []byte) (*http.Response, error) {
 }
 
 func makeGetRequest(url string, headers map[string]string) (map[string]interface{}, error) {
-	resp, err := httpGetWithUA(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GET request: %w", err)
+	}
+
+	// Set default User-Agent
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+
+	// Set additional headers if provided
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("GET request failed: %w", err)
 	}
